90ec211882d6746f335eb60c3c853bbbe1a55719
Woody <nz.wgkim@gmail.com>
2023-03-07 15:56:58 +1300

    FutureProvider.when에서 error 처리

--- lib/riverpod/future_provider.dart
+++ lib/riverpod/future_provider.dart
@@ -4,5 +4,6 @@ final multiplesFutureProvider = FutureProvider<List<int>>((ref) async {
   await Future.delayed(
     const Duration(seconds: 2),
   );
+  throw Exception('에러입니다.');
   return List<int>.generate(10, (index) => index);
 });
