467d0413dc755e9966f83f5b75c8e3351206ac9b
Woody <nz.wgkim@gmail.com>
2023-03-10 12:10:04 +1300

    Consumer와 ConsumerWidet은 ref를 제공받을 수 있고, local하게 build가 가능하다.

--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -8,75 +8,116 @@ class CodeGenerationScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    print('build...');
     final state1 = ref.watch(gStateProvider);
     final autoDisposeState = ref.watch(gStateFutureProvider);
     final keepAliveState = ref.watch(gStateFuture2Provider);
     final codeGenerationFamilyState =
         ref.watch(gFamilyStateProvider(number1: 3, number2: 7));
-    final gstateNotifierProvider = ref.watch(gStateNotifierProvider.notifier);
-    final gstateNotifierProvider2 = ref.watch(gStateNotifierProvider);
+
+    // notifier를 watch한다. notifier에서 제공된, 초기화함수 build(), increase를 사용할 수 있다.
+    // 하지만 build하지는 못한다.
+
+    /// 헉... ref.watch(gStateNotifierProvider); 가 있어야 build 한다.
+
+    final gstateProviderNotifier = ref.read(gStateNotifierProvider.notifier);
+    // final gstateProvider = ref.watch(gStateNotifierProvider);
+
+    // ref.watch(gStateNotifierProvider)은
+    // ref.watch(gStateNotifierProvider.notifier).state와 같다.
+
     return DefaultLayout(
-        title: 'CodeGenerationScreen',
-        body: SizedBox(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'state1:$state1',
+      title: 'CodeGenerationScreen',
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'state1:$state1',
+              style: const TextStyle(fontSize: 20),
+            ),
+            Text(
+              'autoDisposeState:${autoDisposeState.value}',
+              style: const TextStyle(fontSize: 20),
+            ),
+            autoDisposeState.when(
+              data: (data) => Text(
+                'autoDisposeState:$data',
+                textAlign: TextAlign.center,
                 style: const TextStyle(fontSize: 20),
               ),
-              Text(
-                'autoDisposeState:${autoDisposeState.value}',
+              error: (err, stack) => Text('Error: $err'),
+              loading: () => const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+            keepAliveState.when(
+              data: (data) => Text(
+                'keepAliveState:$data',
+                textAlign: TextAlign.center,
                 style: const TextStyle(fontSize: 20),
               ),
-              autoDisposeState.when(
-                data: (data) => Text(
-                  'autoDisposeState:$data',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 20),
-                ),
-                error: (err, stack) => Text('Error: $err'),
-                loading: () => const Center(
-                  child: CircularProgressIndicator(),
-                ),
+              error: (err, stack) => Text('Error: $err'),
+              loading: () => const Center(
+                child: CircularProgressIndicator(),
               ),
-              keepAliveState.when(
-                data: (data) => Text(
-                  'keepAliveState:$data',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 20),
-                ),
-                error: (err, stack) => Text('Error: $err'),
-                loading: () => const Center(
-                  child: CircularProgressIndicator(),
-                ),
-              ),
-              Text(
-                'codeGenerationFamilyState:$codeGenerationFamilyState',
-                style: const TextStyle(fontSize: 20),
-              ),
-              Text(
-                'gstateNotifierProvider:${gstateNotifierProvider.state}',
-                style: const TextStyle(fontSize: 20),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                      onPressed: () => gstateNotifierProvider.increase(),
-                      child: const Text('Increase')),
-                  ElevatedButton(
-                      onPressed: () => gstateNotifierProvider.decrease(),
-                      child: const Text('Decrease')),
-                  ElevatedButton(
-                      onPressed: () => ref.invalidate(gStateNotifierProvider),
-                      child: const Text('invalidate')),
-                ],
-              ),
-              // invalidate...
-            ],
-          ),
-        ));
+            ),
+            Text(
+              'codeGenerationFamilyState:$codeGenerationFamilyState',
+              style: const TextStyle(fontSize: 20),
+            ),
+            // Text(
+            //   'gstateNotifierProvider:${gstateProviderNotifier.state}',
+            //   style: const TextStyle(fontSize: 20),
+            // ),
+
+            // const TestConsumer(),
+
+            /// build를 부르는 것은 ref.watch(gStateNotifierProvider) 이다.
+            /// 이게 Consumer안으로 집어넣으면, local하게 build된다.
+            /// ref를 제공해 주는 widget이 consumer 밖에 없는 듯...
+            ///
+            Consumer(
+              builder: (context, ref, child) {
+                final gstateNotifierProvider =
+                    ref.watch(gStateNotifierProvider);
+                return Text('gstateNotifierProvider:$gstateNotifierProvider');
+              },
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () => gstateProviderNotifier.increase(),
+                    child: const Text('Increase')),
+                ElevatedButton(
+                    onPressed: () => gstateProviderNotifier.decrease(),
+                    child: const Text('Decrease')),
+
+                // invalidate...
+                ElevatedButton(
+                    onPressed: () => ref.invalidate(gStateNotifierProvider),
+                    child: const Text('invalidate')),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TestConsumer extends ConsumerWidget {
+  const TestConsumer({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final gstateProviderConsumer = ref.watch(gStateNotifierProvider);
+    return Text(
+      'gstateNotifierProvider:$gstateProviderConsumer',
+      style: const TextStyle(fontSize: 20),
+    );
   }
 }
